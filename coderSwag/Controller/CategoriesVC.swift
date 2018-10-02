beaf69b72a6645be571502ca419f1901138aeb5d
West Kraemer <kraemerwest@gmail.com>
2018-10-02 18:14:03 -0400

    add iboutlets for prototype cells. Also add launch screen image

---
+++ coderSwag/Controller/CategoriesVC.swift
@@ -0,0 +1,21 @@
+//
+//  CategoriesVC.swift
+//  coderSwag
+//
+//  Created by West Kraemer on 10/2/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+}
---
+++ coderSwag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coderSwag
+//
+//  Created by West Kraemer on 10/2/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
