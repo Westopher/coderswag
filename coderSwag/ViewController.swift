52a42f67819a77dd58d85ee235f9e9033d9651c0
West Kraemer <kraemerwest@gmail.com>
2018-10-02 13:36:13 -0400

    first commit

---
+++ coderSwag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coderSwag
+//
+//  Created by West Kraemer on 10/2/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
