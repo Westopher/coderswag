f4652df753feed315d3f353efefde483faec335f
West Kraemer <kraemerwest@gmail.com>
2018-10-03 17:38:47 -0400

    call data with dataservice on table view cells

--- coderSwag/Controller/CategoriesVC.swift
+++ coderSwag/Controller/CategoriesVC.swift
@@ -14,15 +14,22 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-      
+        categoryTable.dataSource = self
+        categoryTable.delegate = self
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return DataService.instance.getCategories().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            cell.updateViews(category: category)
+            return cell
+        } else {
+            return CategoryCell()
+        }
+        
     }
 }
