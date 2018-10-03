d8e3048bb8c7b7f35340175fe7c52937556685c1
West Kraemer <kraemerwest@gmail.com>
2018-10-03 16:41:32 -0400

    add data model

---
+++ coderSwag/Model/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  coderSwag
+//
+//  Created by West Kraemer on 10/3/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import Foundation
+
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+}
