0ebc16a5c8e15383e35c1d020b8757de8dceb0b9
West Kraemer <kraemerwest@gmail.com>
2018-10-10 16:40:32 -0400

    add dataset to dataservices. Data for products in hardcoded, rather than called from a server.

--- coderSwag/View/ProductCell.swift
+++ coderSwag/View/ProductCell.swift
@@ -11,5 +11,5 @@ import UIKit
 class ProductCell: UICollectionViewCell {
     @IBOutlet weak var productImage: UIImageView!
     @IBOutlet weak var productTitle: UILabel!
-    @IBOutlet weak var productPrice: UILabel
+    @IBOutlet weak var productPrice: UILabel!
 }
