979f9fb39fcf98a857cbcb93ebd16244572b4851
West Kraemer <kraemerwest@gmail.com>
2018-10-10 15:56:28 -0400

    add iboutlets for collection view

---
+++ coderSwag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coderSwag
+//
+//  Created by West Kraemer on 10/10/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImage!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel
+}
