1a8aed7e6ea0430a28eb0c02dff54066f1a63105
West Kraemer <kraemerwest@gmail.com>
2018-10-10 20:36:25 -0400

    add segue prep in category VC

--- coderSwag/Controller/CategoriesVC.swift
+++ coderSwag/Controller/CategoriesVC.swift
@@ -30,6 +30,14 @@ class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate
         } else {
             return CategoryCell()
         }
-        
+    }
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let category = DataService.instance.getCategories()[indexPath.row]
+            performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            
+        }
     }
 }
---
+++ coderSwag/Controller/ProductsVC.swift
@@ -0,0 +1,21 @@
+//
+//  ProductsVC.swift
+//  coderSwag
+//
+//  Created by West Kraemer on 10/10/18.
+//  Copyright © 2018 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    func initProducts 
+    
+
+}
