f07a0eaebc55253f59632ca1b5496223890310ae
West Kraemer <kraemerwest@gmail.com>
2018-10-10 20:48:11 -0400

    add CV delegate and CV datasource

--- coderSwag/Controller/ProductsVC.swift
+++ coderSwag/Controller/ProductsVC.swift
@@ -8,17 +8,34 @@
 
 import UIKit
 
-class ProductsVC: UIViewController {
+class ProductsVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+  
+    
 
     private(set) public var products = [Product]()
+    
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+        
     }
     
     func initProducts(category: Category) {
         products = DataService.instance.getProducts(forCategoryTitle: category.title)
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProduectCell", for: indexPath) {
+            
+        }
+        
+    }
 
 }
